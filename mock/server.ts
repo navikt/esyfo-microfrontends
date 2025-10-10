27d12f14dbb0918bd6ea8b0611a637737127ee3a
Nima <nima.hakimi@nav.no>
2025-10-10 10:49:05 +0200

    Settup mock data

--- mock/server.ts
+++ mock/server.ts
@@ -1,6 +1,15 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { createAvlysningsBrev, createInnkallingsBrev, createReferatBrev, createReferatEndretBrev } from "./data/brev";
+import { ingenMotebehov } from "./data/ingenMotebehov";
+import { motebehovMedSvar } from "./data/motebehovMedSvar";
+import { motebehovUtenSvar } from "./data/motebehovUtenSvar";
+
+const innkalt = createInnkallingsBrev();
+const avlyst = createAvlysningsBrev();
+const referat = createReferatBrev();
+const referatEndret = createReferatEndretBrev();
 
 const api = new Hono();
 
@@ -13,8 +22,12 @@ api.use(
   }),
 );
 
-api.get("/api/tms-astro-template", (c) => {
-  return c.json(example);
+api.get("/api/dialogmote", (c) => {
+  return c.json(innkalt);
+});
+
+api.get("/api/motebehov", (c) => {
+  return c.json(motebehovUtenSvar);
 });
 
 serve(api);
