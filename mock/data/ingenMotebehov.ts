33e3f440a4e6f9e7f038e596f5245e25a9aef70a
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-01-06 10:53:31 +0100

    Fjern ubrukte filer

--- mock/data/ingenMotebehov.ts
+++
@@ -1,7 +0,0 @@
-import type { MotebehovDTO } from "@schema/motebehovSchema";
-
-export const ingenMotebehov: MotebehovDTO = {
-  visMotebehov: false,
-  motebehov: null,
-  skjemaType: null,
-};
--- mock/data/motebehovUtenSvar.ts
+++
@@ -1,7 +0,0 @@
-import type { MotebehovDTO } from "@schema/motebehovSchema";
-
-export const motebehovUtenSvar: MotebehovDTO = {
-  visMotebehov: true,
-  skjemaType: "SVAR_BEHOV",
-  motebehov: null,
-};
