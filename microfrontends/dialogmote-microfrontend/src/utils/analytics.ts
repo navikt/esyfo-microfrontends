555b112002604ce0e7debfed8cb71c4f4ce2ac69
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-02-10 08:27:59 +0100

    Fjern ubrukt import

--- microfrontends/dialogmote-microfrontend/src/utils/analytics.ts
+++ microfrontends/dialogmote-microfrontend/src/utils/analytics.ts
@@ -1,6 +1,5 @@
 import type { AmplitudeEvent } from "@navikt/nav-dekoratoren-moduler";
 import { getAnalyticsInstance } from "@navikt/nav-dekoratoren-moduler";
-import type {SvarTypeDTO} from "../../schema/brevSchema.ts";
 
 type ExtendedAmpltitudeEvent = AmplitudeEvent<"navigere", { lenketekst: string }>;
 
